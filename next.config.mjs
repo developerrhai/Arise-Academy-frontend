dfe77b289d7b15085ef43a0627d18084300ef7ef
nishantitsaoirse-head <nishant.it.saoirse@gmail.com>
2026-07-31 17:56:33 +0530

    chore: hardcode NEXT_PUBLIC_API_URL in next config for Vercel

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  env: {
+    NEXT_PUBLIC_API_URL: 'https://institute-api.rhaitech.online/arise/api',
+  },
   typescript: {
     ignoreBuildErrors: true,
   },
